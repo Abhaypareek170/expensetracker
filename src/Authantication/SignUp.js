a231762405d1296689e407f70de915a1ed98354c
Abhay Pareek <abhaypareek170@gmail.com>
2022-11-09 15:54:18 +0530

    SignUp Page

---
+++ src/Authantication/SignUp.js
@@ -0,0 +1,91 @@
+
+import { useRef, useState } from "react";
+import "./SignUp.modules.css";
+
+
+const AuthForm = () => {
+  const [isSending, setIsSending] = useState(false);
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
+  const cnfPasswordInputRef= useRef();
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const enteredEmail = emailInputRef.current.value;
+    const enteredPassword = passwordInputRef.current.value;
+    setIsSending(true);
+    
+    fetch("https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDPexDNdjOMbM7eoDYU1-DP6ytLvuzTifQ", {
+      method: "POST",
+      body: JSON.stringify({
+        email: enteredEmail,
+        password: enteredPassword,
+        returnSecureToken: true,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => {
+        setIsSending(false);
+        if (res.ok) {
+          console.log("User registered successfully!");
+          return res.json();
+      } else {
+          return res.json().then(() => {
+            let errorMessage = "Failed!";
+            alert(errorMessage);
+            throw new Error(errorMessage);
+          });
+        }
+      })
+  };
+
+  return (
+    <section className="vh-100 bg-image"
+  style={{backgroundImage:`url('https://mdbcdn.b-cdn.net/img/Photos/new-templates/search-box/img4.webp')`}}>
+  <div className="mask d-flex align-items-center h-100 gradient-custom-3">
+    <div className="container h-100">
+      <div className="row d-flex justify-content-center align-items-center h-100">
+        <div className="col-12 col-md-9 col-lg-7 col-xl-6">
+          <div className="card" style={{borderRadius: "15px"}}>
+            <div className="card-body p-5">
+              <h2 className="text-uppercase text-center mb-5">Create an account</h2>
+
+              <form >
+                <div className="form-outline mb-4">
+                  <input type="email" id="form3Example3cg" className="form-control form-control-lg" required ref={emailInputRef} />
+                  <label className="form-label" htmlFor="form3Example3cg">Your Email</label>
+                </div>
+
+                <div className="form-outline mb-4">
+                  <input type="password" id="form3Example4cg" className="form-control form-control-lg" required ref={passwordInputRef}/>
+                  <label className="form-label" htmlFor="form3Example4cg">Password</label>
+                </div>
+
+                <div className="form-outline mb-4">
+                  <input type="password" id="form3Example4cdg" className="form-control form-control-lg" required ref={cnfPasswordInputRef}/>
+                  <label className="form-label" htmlFor="form3Example4cdg">Repeat your password</label>
+                </div>
+
+                <div className="d-flex justify-content-center">
+                  <button type="button"
+                    className="btn btn-success btn-block btn-lg gradient-custom-4 text-body" onClick={submitHandler}>Register</button>
+                </div>
+
+                <p className="text-center text-muted mt-5 mb-0">Have already an account? <a href="#!"
+                    className="fw-bold text-body"><u>Login here</u></a></p>
+
+              </form>
+              {isSending?<p className="centered">Sending Request...</p>:''}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+  );
+};
+
+export default AuthForm;
