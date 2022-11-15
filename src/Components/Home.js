a305f788a5ea773e7a70566fadf618acfdff06f6
Abhay Pareek <abhaypareek170@gmail.com>
2022-11-15 16:17:08 +0530

    Logout

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,7 +1,41 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const navigate = useNavigate();
+  const token = localStorage.getItem('token');
+  const logoutHandler = (e)=>{
+    e.preventDefault();
+    localStorage.removeItem('email');
+    localStorage.removeItem('token')
+    navigate('/login');
+  }
+  const emailVerificationHandler = (e) =>{
+      e.preventDefault();
+      fetch("https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=AIzaSyDPexDNdjOMbM7eoDYU1-DP6ytLvuzTifQ",{
+        method: "POST",
+        body: JSON.stringify({
+          requestType:"VERIFY_EMAIL",
+          idToken:token,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }).then((res)=>{
+        if(res.ok){
+          return res;
+        }
+        else {
+          return res.json().catch((err) => {
+            let errorMessage = "Failed!";
+            alert(errorMessage);
+            throw new Error(err);
+          });
+        }
+      }).then((res)=>{
+        console.log(res)
+      })
+  }
   return (
     <>
       <div style={{display:'flex',justifyContent:'space-between'}}>
@@ -10,11 +44,16 @@ const Home = () => {
             <b>Welcome to Expense Tracker</b>
           </i>
         </span>
+        <span><button onClick={logoutHandler}>Logout</button></span>
         <span>
           Your profile is incomplete.<Link to="/updateProfile">Complete now</Link>
         </span>
       </div>
       <hr />
+      <div>
+        <h1><button onClick={emailVerificationHandler}>Verify your mail.</button></h1>
+      </div>
+      
     </>
   );
 };
