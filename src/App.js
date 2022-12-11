619a709f562813d0de8f4d3b75ede3b7cb57c9e3
Abhay Pareek <abhaypareek170@gmail.com>
2022-12-11 21:39:52 +0530

    Format Docs

--- src/App.js
+++ src/App.js
@@ -12,13 +12,16 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/signup" element={<SignUp/>}></Route>
-        <Route path="/login" element={<Login/>}></Route>
-        <Route path="/" element={<Protected Component={Home}/>}></Route>
-        <Route path="/updateProfile" element={<UpdateProfile/>}></Route>
-        <Route path="/forgot-password" element={<ForgotPassword/>}></Route>
-        <Route path="/expenses" element={<Protected Component={Expense}/>}></Route>
-        <Route path="*" element={<Protected Component={NotFound}/>}></Route>
+        <Route path="/signup" element={<SignUp />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/" element={<Protected Component={Home} />}></Route>
+        <Route path="/updateProfile" element={<UpdateProfile />}></Route>
+        <Route path="/forgot-password" element={<ForgotPassword />}></Route>
+        <Route
+          path="/expenses"
+          element={<Protected Component={Expense} />}
+        ></Route>
+        <Route path="*" element={<Protected Component={NotFound} />}></Route>
       </Routes>
     </BrowserRouter>
   );
